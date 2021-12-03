c798661912d560410740112e1e3e86e753c19f9b
Andrei Zavgorodnii <andrei.z@p2p.org>
2021-12-03 13:37:47 +0300

    feat: config and refactoring

--- main.go
+++ main.go
@@ -4,7 +4,13 @@ import (
 	"context"
 	"encoding/binary"
 	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/herumi/bls-eth-go-binary/bls"
 	e2wallet "github.com/wealdtech/go-eth2-wallet"
@@ -14,8 +20,134 @@ import (
 	e2wtypes "github.com/wealdtech/go-eth2-wallet-types/v2"
 )
 
-// BLSID turns a uint64 in to a BLS identifier.
-func BLSID(id uint64) *bls.ID {
+const MasterPrivateKeyEnvName = "MASTER_PRIVATE_KEY"
+
+var (
+	configPath = flag.String("config", "./config.json", "Path to the config file."+
+		" Note that the secret key should be provided in an environment variable")
+)
+
+func init() {
+	if err := bls.Init(bls.BLS12_381); err != nil {
+		log.Fatalf("Failed to Init: %s", err)
+	}
+
+	if err := bls.SetETHmode(bls.EthModeDraft07); err != nil {
+		log.Fatalf("Failed to SetETHmode: %s", err)
+	}
+}
+
+func saveWallets(config *Config, masterPKs []bls.PublicKey, participantsSKs []bls.SecretKey) error {
+	ctx := context.Background()
+	//todo remove when debugging ends
+	encryptor := keystorev4.New()
+
+	participantsMap := make(map[uint64]string)
+	for _, participant := range config.Participants {
+		participantsMap[participant.Id] = participant.Uri
+	}
+
+	accountName := masterPKs[0].SerializeToHexStr()[:8]
+	verificationVector := bytesFromPKSlice(masterPKs) // cast to [][]byte{
+
+	for idx := 0; idx < len(config.Participants); idx++ {
+		err := func() error {
+			currentStore := fmt.Sprintf("%s%s%d", config.OutputDir, "/", config.Participants[idx].Id)
+			store := filesystem.New(filesystem.WithLocation(currentStore))
+			if err := e2wallet.UseStore(store); err != nil {
+				return fmt.Errorf("failed to UseStore: %w", err)
+			}
+
+			if _, err := store.RetrieveWallet(config.WalletName); err != nil {
+				if _, err := distributed.CreateWallet(ctx, config.WalletName, store, encryptor); err != nil {
+					return fmt.Errorf("failed to CreateWallet: %w", err)
+				}
+			}
+
+			// Open a wallet
+			currentWallet, err := e2wallet.OpenWallet(config.WalletName)
+			if err != nil {
+				return fmt.Errorf("failed to OpenWallet: %w", err)
+			}
+
+			err = currentWallet.(e2wtypes.WalletLocker).Unlock(context.Background(), nil)
+			if err != nil {
+				return fmt.Errorf("failed to Unlock wallet: %w", err)
+			}
+			// Immediately defer locking the wallet to ensure it does not remain unlocked outside the function.
+			defer func(locker e2wtypes.WalletLocker, ctx context.Context) {
+				err := locker.Lock(ctx)
+				if err != nil {
+					log.Printf("Failed to Lock the wallet for participant %s", config.Participants[idx].Uri)
+				}
+			}(currentWallet.(e2wtypes.WalletLocker), context.Background())
+
+			currentPassphrase := []byte(config.Participants[idx].Passphrase)
+
+			_, err = currentWallet.(e2wtypes.WalletDistributedAccountImporter).ImportDistributedAccount(context.Background(),
+				accountName,
+				participantsSKs[idx].Serialize(),
+				uint32(config.Threshold),
+				verificationVector,
+				participantsMap,
+				currentPassphrase) //don't remember what that is; investigate
+			if err != nil {
+				return fmt.Errorf("failed to ImportDistributedAccount: %w", err)
+			}
+
+			return nil
+		}()
+		if err != nil {
+			return fmt.Errorf("failed to save wallet for participant %d: %w", idx, err)
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	config, masterPrivateKeyStr, err := ReadConfig()
+	if err != nil {
+		log.Fatalf("Failed to ReadConfig: %s", err)
+	}
+
+	var masterSK bls.SecretKey
+	masterSKByte, _ := hex.DecodeString(masterPrivateKeyStr)
+	if err := masterSK.Deserialize(masterSKByte); err != nil {
+		log.Fatalf("Failed to Deserialize master private key: %s", err)
+	}
+
+	// todo do domain
+	msg := []byte("abc")
+	masterSignedMsg := masterSK.SignByte(msg)
+	if masterSignedMsg.VerifyByte(masterSK.GetPublicKey(), msg) {
+		log.Println("successfully verified master SK signature")
+	} else {
+		log.Fatal("failed to verify master SK signature")
+	}
+
+	masterSKs, masterPKs := setupMasterKeys(config, masterSK)
+
+	log.Printf("master pub %s\n", masterSK.GetPublicKey().SerializeToHexStr())
+
+	participantsIDs, participantsSKs, participantsPKs, signatures := setupParticipants(config, masterSKs, masterPKs, msg)
+
+	if err := checkKeys(config, masterSK, msg, participantsIDs, participantsSKs, participantsPKs, signatures); err != nil {
+		log.Fatalf("failed to checkKeys: %s", err)
+	}
+	log.Println("keys check success")
+
+	if err := saveWallets(config, masterPKs, participantsSKs); err != nil {
+		log.Fatalf("failed to saveWallets: %s", err)
+	}
+
+	log.Printf("wallet saved to %s", config.OutputDir)
+}
+
+// newBlsID turns a uint64 in to a BLS identifier.
+func newBlsID(id uint64) *bls.ID {
 	var res bls.ID
 	buf := [8]byte{}
 	binary.LittleEndian.PutUint64(buf[:], id)
@@ -33,104 +165,123 @@ func bytesFromPKSlice(PKSlice []bls.PublicKey) [][]byte {
 	return ret
 }
 
-func main() {
-	bls.Init(bls.BLS12_381)
-	bls.SetETHmode(bls.EthModeDraft07)
+type Participant struct {
+	Id         uint64 `json:"id"`
+	Uri        string `json:"uri"`
+	Passphrase string `json:"passphrase"`
+}
 
-	//todo: take input w/ commandline or json file, whatever is easier
-	//todo: take as an input a private key
-	//todo: take as an input participants json
-	//todo: take as an input signing threshold
-	//todo: usage readme
-	//optional:
-	//todo: sanity checks on inputs
-	//todo: clean up generally
+type Config struct {
+	Threshold    int           `json:"threshold"`
+	OutputDir    string        `json:"output_dir"`
+	WalletName   string        `json:"wallet_name"`
+	Participants []Participant `json:"participants"`
+}
 
-	type participant struct {
-		id         uint64
-		uri        string
-		passphrase string
+func ReadConfig() (config *Config, masterPrivateKey string, err error) {
+	configBz, err := ioutil.ReadFile(*configPath)
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to ReadFile (config: %s): %s", *configPath, err)
 	}
 
-	//inputs start
-
-	outdir := "distwallets"
-	walletname := "distrib"
-
-	masterPrivateKeyStr := "3eb84bbe03db1c6341c490142a647655f33983ed693d0f43c696ed0378fdc492"
-
-	participants := []participant{
-		{70358052, "solana-multisig-1:8881", "secret1"},
-		{46192271, "solana-multisig-2:8882", "secret2"},
-		{76680527, "solana-multisig-3:8883", "secret3"},
+	if err := json.Unmarshal(configBz, &config); err != nil {
+		return nil, "", fmt.Errorf("failed to Unmarshal config: %s", err)
 	}
 
-	signingThreshold := 2
+	if len(config.Participants) < 2 {
+		return nil, "", errors.New("participants count should be greater than 1")
+	}
 
-	//inputs end
+	if len(config.OutputDir) == 0 {
+		return nil, "", errors.New("output directory not specified")
+	}
 
-	//todo: count number of participants
-	participantsCount := 3
+	if len(config.WalletName) == 0 {
+		return nil, "", errors.New("wallet name not specified")
+	}
 
-	var masterSK bls.SecretKey
-	masterSKByte, _ := hex.DecodeString(masterPrivateKeyStr)
-	masterSK.Deserialize(masterSKByte)
+	if config.Threshold > len(config.Participants) || config.Threshold == 0 {
+		return nil, "", fmt.Errorf("invalid threshold value %d (should be between 1 and %d)",
+			config.Threshold, len(config.Participants))
+	}
 
-	//only 1st position in this two arrays matters
-	masterSKs := []bls.SecretKey{}
-	masterPKs := []bls.PublicKey{}
-	masterSKs = append(masterSKs, masterSK)
+	// masterPrivateKeyStr := "3eb84bbe03db1c6341c490142a647655f33983ed693d0f43c696ed0378fdc492"
 
-	paritcipantsIDs := []bls.ID{}
-	participantsSKs := []bls.SecretKey{}
-	participantsPKs := []bls.PublicKey{}
-	signatures := []bls.Sign{}
+	masterPrivateKey, ok := os.LookupEnv(MasterPrivateKeyEnvName)
+	if !ok {
+		return nil, "", fmt.Errorf("MASTER_PRIVATE_KEY environment variable not specified")
+	}
+	if len(masterPrivateKey) != 64 {
+		return nil, "", errors.New("invalid MASTER_PRIVATE_KEY")
+	}
 
-	for i := 1; i < signingThreshold; i++ {
+	return
+}
+
+func setupParticipants(config *Config, masterSKs []bls.SecretKey, masterPKs []bls.PublicKey, msg []byte) (
+	participantsIDs []bls.ID,
+	participantsSKs []bls.SecretKey,
+	participantsPKs []bls.PublicKey,
+	signatures []bls.Sign,
+) {
+	for i := 0; i < len(config.Participants); i++ {
+		id := newBlsID(config.Participants[i].Id)
+
+		participantsIDs = append(participantsIDs, *id)
 		var sk bls.SecretKey
-		sk.SetByCSPRNG() //this actually doesn't matter
-		masterSKs = append(masterSKs, sk)
-	}
-
-	masterPKs = bls.GetMasterPublicKey(masterSKs)
-
-	fmt.Printf("master priv %s\n", masterSK.SerializeToHexStr())
-	fmt.Printf("master pub %s\n", masterSK.GetPublicKey().SerializeToHexStr())
-
-	//todo do domain
-	msg := []byte("abc")
-
-	msg_sig := masterSK.SignByte(msg)
-
-	fmt.Printf("verify=%v\n", msg_sig.VerifyByte(masterSK.GetPublicKey(), msg))
-
-	for i := 0; i < participantsCount; i++ {
-		id := BLSID(participants[i].id)
-
-		paritcipantsIDs = append(paritcipantsIDs, *id)
-		var sk bls.SecretKey
-		sk.Set(masterSKs, id)
+		if err := sk.Set(masterSKs, id); err != nil {
+			log.Fatalf("Failed to Set secret key: %s", err)
+		}
 		participantsSKs = append(participantsSKs, sk)
 
 		var pk bls.PublicKey
-		pk.Set(masterPKs, id)
+		if err := pk.Set(masterPKs, id); err != nil {
+			log.Fatalf("Failed to Set public key: %s", err)
+		}
 		participantsPKs = append(participantsPKs, pk)
 
 		sig := sk.SignByte(msg)
 		signatures = append(signatures, *sig)
 	}
 
-	idxss := [][]uint32{{1, 2}, {0, 2}, {0, 1}}
+	return
+}
 
-	for _, idxs := range idxss {
-		subIDs := []bls.ID{}
-		subSKs := []bls.SecretKey{}
-		subPKs := []bls.PublicKey{}
-		subSigs := []bls.Sign{}
+func setupMasterKeys(config *Config, masterSK bls.SecretKey) (masterSKs []bls.SecretKey, masterPKs []bls.PublicKey) {
+	masterSKs = append(masterSKs, masterSK)
 
-		for i := 0; i < signingThreshold; i++ {
-			idx := idxs[i]
-			subIDs = append(subIDs, paritcipantsIDs[idx])
+	for i := 1; i < config.Threshold; i++ {
+		var sk bls.SecretKey
+		sk.SetByCSPRNG() // This actually doesn't matter.
+		masterSKs = append(masterSKs, sk)
+	}
+
+	masterPKs = bls.GetMasterPublicKey(masterSKs)
+
+	return
+}
+
+func checkKeys(
+	config *Config,
+	masterSK bls.SecretKey,
+	msg []byte,
+	participantsIDs []bls.ID,
+	participantsSKs []bls.SecretKey,
+	participantsPKs []bls.PublicKey,
+	signatures []bls.Sign,
+) error {
+	indexPairs := [][]uint32{{1, 2}, {0, 2}, {0, 1}}
+	for idx, indexPair := range indexPairs {
+		var (
+			subIDs  []bls.ID
+			subSKs  []bls.SecretKey
+			subPKs  []bls.PublicKey
+			subSigs []bls.Sign
+		)
+
+		for i := 0; i < config.Threshold; i++ {
+			idx := indexPair[i]
+			subIDs = append(subIDs, participantsIDs[idx])
 			subSKs = append(subSKs, participantsSKs[idx])
 			subPKs = append(subPKs, participantsPKs[idx])
 			subSigs = append(subSigs, signatures[idx])
@@ -140,67 +291,22 @@ func main() {
 		var pub bls.PublicKey
 		var sig bls.Sign
 
-		sec.Recover(subSKs, subIDs)
-		fmt.Printf("recover priv %s\n", sec.SerializeToHexStr())
-		pub.Recover(subPKs, subIDs)
-		fmt.Printf("recover pub %s\n", pub.SerializeToHexStr())
-		sig.Recover(subSigs, subIDs)
-		fmt.Printf("verify=%v\n", sig.VerifyByte(masterSK.GetPublicKey(), msg))
+		if err := sec.Recover(subSKs, subIDs); err != nil {
+			return fmt.Errorf("failed to Recover priv: %w", err)
+		}
+
+		if err := pub.Recover(subPKs, subIDs); err != nil {
+			return fmt.Errorf("failed to Recover pub: %w", err)
+		}
+
+		if err := sig.Recover(subSigs, subIDs); err != nil {
+			return fmt.Errorf("failed to Recover signature: %w", err)
+		}
+
+		if !sig.VerifyByte(masterSK.GetPublicKey(), msg) {
+			return fmt.Errorf("failed to verify signature for index pair %d", idx)
+		}
 	}
 
-	// below is pseudocode, does not compile
-
-	ctx := context.Background()
-	//todo remove when debugging ends
-	encryptor := keystorev4.New()
-
-	participantsMap := make(map[uint64]string)
-	for _, participant := range participants {
-		participantsMap[participant.id] = participant.uri
-	}
-
-	accountName := masterPKs[0].SerializeToHexStr()[:8]
-	verificationVector := bytesFromPKSlice(masterPKs) // cast to [][]byte{
-
-	for i := 0; i < participantsCount; i++ {
-		currentStore := fmt.Sprintf("%s%s%d", outdir, "/", participants[i].id)
-		store := filesystem.New(filesystem.WithLocation(currentStore))
-		e2wallet.UseStore(store)
-
-		if _, err := store.RetrieveWallet(walletname); err != nil {
-			if _, err := distributed.CreateWallet(ctx, walletname, store, encryptor); err != nil {
-				panic(err)
-			}
-		}
-
-		// Open a wallet
-		currentWallet, err := e2wallet.OpenWallet(walletname)
-		if err != nil {
-			panic(err)
-		}
-
-		err = currentWallet.(e2wtypes.WalletLocker).Unlock(context.Background(), nil)
-		if err != nil {
-			panic(err)
-		}
-		// Always immediately defer locking the wallet to ensure it does not remain unlocked outside of the function.
-		defer currentWallet.(e2wtypes.WalletLocker).Lock(context.Background())
-
-		currentPassphrase := []byte(participants[i].passphrase)
-
-		_, err = currentWallet.(e2wtypes.WalletDistributedAccountImporter).ImportDistributedAccount(context.Background(),
-			accountName,
-			participantsSKs[i].Serialize(),
-			uint32(signingThreshold),
-			verificationVector,
-			participantsMap,
-			currentPassphrase) //don't remember what that is; investigate
-		if err != nil {
-			panic(err)
-		}
-
-		currentWallet.(e2wtypes.WalletLocker).Lock(context.Background())
-
-	}
-
+	return nil
 }
